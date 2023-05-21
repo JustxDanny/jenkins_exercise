0dec73c418daa49e6bbbd30adccc3143044cc305
JustxDanny <123653035+JustxDanny@users.noreply.github.com>
2023-05-21 18:05:03 +0300

    Update main.tf - added public pubket

--- main.tf
+++ main.tf
@@ -11,3 +11,27 @@ resource "aws_instance" "jenkins_example" {
 resource "aws_s3_bucket" "mys3"{
     bucket = "terraform-bucket-daniel"
 }
+resource "aws_s3_bucket" "bucket_public" {
+  bucket = "terraform-bucket-daniel"
+  
+  # Other bucket configuration options...
+
+  # Enable public access
+  acl = "public-read"
+
+  # Add a bucket policy to allow public access
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::your-bucket-name/*"
+    }
+  ]
+}
+EOF
+}
